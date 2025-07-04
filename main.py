075b4e9fd0396415f4284a404fb8da850142d370
LucasRnbc <lucas.robertonascimento01@gmail.com>
2025-07-03 21:09:11 -0300

    Atualização com interface web que interage com a API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Dict
 import joblib
@@ -7,6 +8,15 @@ import datetime
 
 modelo = joblib.load("app/model/modelo_noticias.joblib")
 app = FastAPI()
+
+# Configuração do CORS para permitir requisições
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 historico: List[Dict] = []
 
@@ -22,6 +32,27 @@ print("🟡 Carregando modelo...")
 modelo = joblib.load("app/model/modelo_noticias.joblib")
 print("🟢 Modelo carregado com sucesso!")
 
+def palavras_mais_influentes(modelo_pipeline, texto, top_n=5):
+    try:
+        vectorizer = modelo_pipeline.named_steps['tfidfvectorizer']
+        classifier = modelo_pipeline.named_steps['logisticregression']
+    except Exception as e:
+        print("Erro ao extrair vetorizador/classificador:", e)
+        return []
+
+    vetor = vectorizer.transform([texto])
+    coef = classifier.coef_[0]
+    indices_ativos = vetor.nonzero()[1]
+
+    palavras_pesos = []
+    for idx in indices_ativos:
+        palavra = vectorizer.get_feature_names_out()[idx]
+        peso = coef[idx]
+        palavras_pesos.append((palavra, peso))
+
+    palavras_ordenadas = sorted(palavras_pesos, key=lambda x: abs(x[1]), reverse=True)
+
+    return [p[0] for p in palavras_ordenadas[:top_n]]
 
 @app.post("/api/classificar-noticia", response_model=RespostaClassificacao)
 def classificar_noticia(req: RequisicaoNoticia):
@@ -32,8 +63,7 @@ def classificar_noticia(req: RequisicaoNoticia):
     probabilidade = float(probas[classe_idx])
     classe = "verdadeira" if classe_idx == 1 else "falsa"
 
-    palavras_chave = ["escândalo", "urgente", "exclusivo"]
-    palavras_influentes = [p for p in palavras_chave if p in texto.lower()]
+    palavras_influentes = palavras_mais_influentes(modelo, texto, top_n=5)
 
     resultado = {
         "classe": classe,
